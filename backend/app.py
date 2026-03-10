895b366affbff2ba6be0ed9a267cc0f5f180310e
Pratham Prajapati <prathamprajapati4071@gmail.com>
2026-03-10 15:18:44 +0530

    In app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from auth_service import authenticate_user, log_login, generate_otp, send_otp_email, store_otp, verify_otp, supabase
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='')
 CORS(app)
