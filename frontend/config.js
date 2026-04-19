f4137f2e8b7e8c3642373d1e2bde24a4c733d878
Pratham Prajapati <prathamprajapati4071@gmail.com>
2026-04-19 19:50:37 +0530

    Update the config.js for deployment

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     // Replace this with your Render backend URL when deployed
     // e.g., 'https://proactive-equipment-care-backend.onrender.com'
-    API_BASE_URL: 'http://localhost:5000'
+    API_BASE_URL: 'http://13.126.238.21'
 };
