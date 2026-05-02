baeab40540a28cb2eb643e9f9259ed0ab9234ff5
Pratham Prajapati <prathamprajapati4071@gmail.com>
2026-05-02 13:45:09 +0530

    Updated config.js for host

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    // API_BASE_URL: 'http://13.126.238.21'
-    API_BASE_URL: 'http://127.0.0.1:5000'
+    API_BASE_URL: 'http://13.126.238.21'
+        // API_BASE_URL: 'http://127.0.0.1:5000'
 
 };
