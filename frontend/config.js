ace834e8e8487acbbf503d22e4aa3653b57394d8
Pratham Prajapati <prathamprajapati4071@gmail.com>
2026-05-02 12:39:27 +0530

    Updated final Report and PPT

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    // Replace this with your Render backend URL when deployed
-    // e.g., 'https://proactive-equipment-care-backend.onrender.com'
-    API_BASE_URL: 'http://13.126.238.21'
+    // API_BASE_URL: 'http://13.126.238.21'
+    API_BASE_URL: 'http://127.0.0.1:5000'
+
 };
